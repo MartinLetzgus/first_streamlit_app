82caaf1c7ddc654616cda03e1a90f033db8e34a6
Martin Letzgus <42713026+MartinLetzgus@users.noreply.github.com>
2022-07-05 11:53:14 +0200

    Update streamlit_app.py
    custom items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 
 streamlit.title("My parents new healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocket Smoothie")
-streamlit.text("Hard-Boiled Fre-Range Eggs")
+streamlit.header("🥣 Breakfast Menu")
+streamlit.text("🥗 Omega 3 and Blueberry Oatmeal")
+streamlit.text("🐔 Kale, Spinach and Rocket Smoothie")
+streamlit.text("🥑🍞 Hard-Boiled Fre-Range Eggs")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
