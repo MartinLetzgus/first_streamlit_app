2333eda19aa59904b23484565efcfc9da2c67904
Martin Letzgus <42713026+MartinLetzgus@users.noreply.github.com>
2022-07-05 11:51:25 +0200

    Update streamlit_app.py
    new items in menue

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title("My parents new healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket Smoothie")
+streamlit.text("Hard-Boiled Fre-Range Eggs")
